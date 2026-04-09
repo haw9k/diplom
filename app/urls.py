1ac75cd26e55c5e1393d2d84bd2ddb2b298dd03b
arsen <haw9kbtw@gmail.com>
2026-04-10 00:37:35 +0300

    Add SMTP test endpoint

--- app/urls.py
+++ app/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
-
+from ui import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('ui.urls')),
     path('accounts/', include('allauth.urls')),
+    path("test-email/", views.test_email, name="test_email"),
 ]
