6766980431ed75b88934fd02be13b3facd39bdf1
arsen <haw9kbtw@gmail.com>
2026-04-09 20:18:37 +0300

    Create superuser during Render build

--- build.sh
+++ build.sh
@@ -5,3 +5,4 @@ python -m pip install --upgrade pip
 pip install -r requirements.txt
 python manage.py collectstatic --noinput
 python manage.py migrate
+python manage.py shell -c "from django.contrib.auth import get_user_model; import os; User=get_user_model(); username=os.getenv('DJANGO_SUPERUSER_USERNAME'); email=os.getenv('DJANGO_SUPERUSER_EMAIL'); password=os.getenv('DJANGO_SUPERUSER_PASSWORD'); User.objects.filter(username=username).exists() or User.objects.create_superuser(username, email, password)"
